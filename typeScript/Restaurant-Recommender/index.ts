fd1fbd2f7431856260f6bff696b6a716dedcb850
Ramona <ramona.ekanayake@appliedtechnology.se>
2023-10-11 11:08:44 +0200

    Implement Filter Function

--- typeScript/Restaurant-Recommender/index.ts
+++ typeScript/Restaurant-Recommender/index.ts
@@ -1,42 +1,33 @@
-export default [
-  {
-    name: "Silver Rice Sushi 🍣",
-    priceBracket: "2",
-    deliveryTimeMinutes: 40,
-    openHour: "12",
-    closeHour: "23",
-    distance: "5",
-  },
-  {
-    name: "Nikko's Rotisserie Chicken 🍗",
-    priceBracket: "1",
-    deliveryTimeMinutes: 20,
-    openHour: "12",
-    closeHour: "21",
-    distance: "8",
-  },
-  {
-    name: "Aita Trattoria 🍝",
-    priceBracket: "3",
-    deliveryTimeMinutes: 60,
-    openHour: "18",
-    closeHour: "22",
-    distance: "1",
-  },
-  {
-    name: "Lula Bagel 🥯",
-    priceBracket: "1",
-    deliveryTimeMinutes: 20,
-    openHour: "0",
-    closeHour: "12",
-    distance: "2",
-  },
-  {
-    name: "Golden Chopstick 🥢",
-    priceBracket: "1",
-    deliveryTimeMinutes: 20,
-    openHour: "15",
-    closeHour: "23",
-    distance: "12",
-  },
-];
+import restaurants from "./restaurants";
+
+const dollarSigns = "$$";
+const deliveryTimeMax = 90;
+const maxDistance = 10;
+const hour: number = new Date().getHours();
+let result;
+
+const priceBracket: number = dollarSigns.length;
+
+const filteredRestaurants = restaurants.filter((restaurant) => {
+  if (Number(restaurant.priceBracket) > priceBracket) {
+    return false;
+  }
+
+  if (Number(restaurant.deliveryTimeMinutes > deliveryTimeMax)) {
+    return false;
+  }
+
+  if (Number(restaurant.distance) > maxDistance) {
+    return false;
+  }
+
+  return restaurant;
+});
+//At the top of index.ts, declare a variable named hour with a value of new Date().getHours(). The getHours method will return the current hour in 24 hour UTC time. Then annotate this variable with the correct type.
+if (filteredRestaurants.length === 0) {
+  result = "There are no restaurants available right now.";
+} else {
+  result = `We found ${filteredRestaurants.length} restaurants, the first is ${filteredRestaurants[0].name}.`;
+}
+
+console.log(result);
